8481b7bbd1e774f9d025979a70146a5ea5b0e9f7
markanthonyuy <macmac.uy@gmail.com>
2018-10-18 13:04:46 +0800

    update url

---
+++ precache-manifest.b0ef5870752a0956fe8923a93830400b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "659ef7ae387291762dca32b11fcddb9a",
+    "url": "/palitan/static/media/repeat.659ef7ae.svg"
+  },
+  {
+    "revision": "088ce9c4d9601f413618",
+    "url": "/palitan/static/js/runtime~main.088ce9c4.js"
+  },
+  {
+    "revision": "449c2f8564db1f3c19a0",
+    "url": "/palitan/static/js/main.449c2f85.chunk.js"
+  },
+  {
+    "revision": "c381929672510ca4837c",
+    "url": "/palitan/static/js/1.c3819296.chunk.js"
+  },
+  {
+    "revision": "449c2f8564db1f3c19a0",
+    "url": "/palitan/static/css/main.d261c765.chunk.css"
+  },
+  {
+    "revision": "fafab746917e5c04fd4cc0dd8f762b38",
+    "url": "/palitan/index.html"
+  }
+];
